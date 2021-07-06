cb37b69809f5d221c5e833cf00ed7aca9c8db7fc
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-06 14:45:44 +0700

    Add files via upload

--- MusicKen/__main__.py
+++ MusicKen/__main__.py
@@ -1,1 +1,19 @@
+import requests
+from pyrogram import Client as Bot
 
+from MusicKen.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from MusicKen.services.callsmusic import run
+
+response = requests.get(BG_IMAGE)
+with open("./etc/foreground.png", "wb") as file:
+    file.write(response.content)
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="MusicKen.modules"),
+)
+
+bot.start()
+run()
---
+++ MusicKen/config.py
@@ -0,0 +1,30 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "musikkuchannel")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/23a3b3708c7e1c04e1dfb.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "GeezMusicBot")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "musikkugroup")
+PROJECT_NAME = getenv("PROJECT_NAME", "MUSIK KU")
+OWNER = getenv("OWNER", "@VckyouuBitch")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/kenkannih/Music-KenKan")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
